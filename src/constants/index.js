e77ef0ced49c9b820f2cadab37db0acec3d9f38d
Saugat <saugat.balami@gmail.com>
2023-12-23 22:36:37 +0545

    done

--- src/constants/index.js
+++ src/constants/index.js
@@ -50,19 +50,19 @@ export const navLinks = [
 
 const services = [
   {
-    title: "Web Developer",
+    title: "Laravel Developer",
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "Django Developer",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "Software Engineer",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Java Developer",
     icon: creator,
   },
 ];
@@ -133,93 +133,93 @@ const experiences = [
 const testimonials = [
   {
     testimonial:
-      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-    name: "Sara Lee",
+      "I thought it was impossible to make a website as beautiful as our product, but Saugat proved me wrong.",
+    name: "Palisa Manandhar",
     designation: "CFO",
     company: "Acme Co",
-    image: "https://randomuser.me/api/portraits/women/4.jpg",
+    image: "https://scontent.fktm8-1.fna.fbcdn.net/v/t39.30808-6/402026774_1541888983013280_5616070620545153664_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=jFS0QlmFRT8AX_iaYjH&_nc_ht=scontent.fktm8-1.fna&oh=00_AfCc4G4IyPIZxUy6bBiZ1RQnzc9cnY6mZf1cX7HePf5wwQ&oe=658C9E30",
   },
   {
     testimonial:
-      "I've never met a web developer who truly cares about their clients' success like Rick does.",
-    name: "Chris Brown",
+      "I've never met a web developer who truly cares about their clients' success like Saugat does.",
+    name: "Sudarshan Bhandari",
     designation: "COO",
     company: "DEF Corp",
-    image: "https://randomuser.me/api/portraits/men/5.jpg",
+    image: "https://scontent.fktm8-1.fna.fbcdn.net/v/t1.6435-1/160554798_255785502691618_75074302211227048_n.jpg?stp=c0.0.200.200a_dst-jpg_p200x200&_nc_cat=100&ccb=1-7&_nc_sid=0222cc&_nc_ohc=uYqGgCWRAXUAX8nqIIt&_nc_ht=scontent.fktm8-1.fna&oh=00_AfBBx2u1VRadq5vMxQUzvT5wujq_b9Y1ddqMRcNivfSUcg&oe=65AE6A59",
   },
   {
     testimonial:
-      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-    name: "Lisa Wang",
+      "After Saugat optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Avishek Rokka",
     designation: "CTO",
     company: "456 Enterprises",
-    image: "https://randomuser.me/api/portraits/women/6.jpg",
+    image: "https://scontent.fktm8-1.fna.fbcdn.net/v/t39.30808-6/403986616_866596815023060_3879687675377624817_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=cE3ROvo3K3AAX8osufF&_nc_oc=AQmIwzow8j26avB3DwEV7OqXZbVkVjbaMC0tgiXLMf3DZVjorsUa7vTsLsvEtpSmcQeeMbmoriNAlbsIRsofshE4&_nc_ht=scontent.fktm8-1.fna&oh=00_AfBU_MXlKkwvgvuxSiHnl2tGhm-qwu1iCc_gnG6Kj4D6VQ&oe=658C75ED",
   },
 ];
 
 const projects = [
   {
-    name: "Car Rent",
+    name: "E-commerce",
     description:
-      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    "An e-commerce platform leveraging Machine Learning (ML) for recommendation systems offers personalized product suggestions based on user behavior, preferences, and historical data.",
     tags: [
       {
-        name: "react",
+        name: "PostgreSQL",
         color: "blue-text-gradient",
       },
       {
-        name: "mongodb",
+        name: "Django",
         color: "green-text-gradient",
       },
       {
-        name: "tailwind",
+        name: "Tailwind",
         color: "pink-text-gradient",
       },
     ],
     image: carrent,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/TheMidnight1/thriftstore",
   },
   {
-    name: "Job IT",
+    name: "Content Management System",
     description:
-      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    "Our CMS enables seamless user management by facilitating user addition, role assignment, and page creation. Effortlessly navigate user roles, assign permissions, and generate content-rich pages, streamlining the content management process.",
     tags: [
       {
-        name: "react",
+        name: "Bootstrap",
         color: "blue-text-gradient",
       },
       {
-        name: "restapi",
+        name: "Laravel",
         color: "green-text-gradient",
       },
       {
-        name: "scss",
+        name: "Jquery",
         color: "pink-text-gradient",
       },
     ],
     image: jobit,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/TheMidnight1/Content-Management-System",
   },
   {
-    name: "Trip Guide",
+    name: "Gym Management System",
     description:
-      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    "Streamlining gym operations, our management system offers efficient membership handling, scheduling, and workout tracking. Seamlessly manage memberships, schedules, and progress, empowering fitness centers to optimize client experiences.",
     tags: [
       {
-        name: "nextjs",
+        name: "PHP",
         color: "blue-text-gradient",
       },
       {
-        name: "supabase",
+        name: "Bootstrap",
         color: "green-text-gradient",
       },
       {
-        name: "css",
+        name: "Jquery",
         color: "pink-text-gradient",
       },
     ],
     image: tripguide,
-    source_code_link: "https://github.com/",
+    source_code_link: "https://github.com/TheMidnight1/Gym-management-System",
   },
 ];
 
