3aef3749d356bb56c8de8a7a50e041a88abf42e2
Saugat <saugat.balami@gmail.com>
2023-12-24 06:33:26 +0545

    Hello

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -2,6 +2,7 @@ import React, { Suspense, useState, useEffect } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
+import logo from '../../assets/logo.svg'
 
 const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
@@ -46,16 +47,19 @@ const ComputersCanvas = () => {
   
   return (
 
-    <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-        />
-        <Computers />
-      </Suspense>
-      <Preload all />
-    </Canvas>
+    // <Canvas frameloop='demand' shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+    //   <Suspense fallback={<CanvasLoader />}>
+    //     <OrbitControls enableZoom={false}
+    //       maxPolarAngle={Math.PI / 2}
+    //       minPolarAngle={Math.PI / 2}
+    //     />
+    //     <Computers />
+    //   </Suspense>
+    //   <Preload all />
+    // </Canvas>
+    <div>
+      Hello
+    </div>
   )
 }
 export default ComputersCanvas
